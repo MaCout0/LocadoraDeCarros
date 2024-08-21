985ad79323d9148298a2150424ec5f5dd573307e
MaCout0 <jkmbint@gmail.com>
2024-08-21 16:37:33 -0300

    serviços grupos de automoveis

---
+++ LocadoraDeCarros.Aplicação/Servicos/GrupoDeAutomoveisService.cs
@@ -0,0 +1,65 @@
+﻿using LocadoraDeCarros.Dominio.ModuloGrupoDeAutomovel;
+using FluentResults;
+
+namespace LocadoraDeCarros.Aplicação.Servicos;
+
+public class GrupoDeAutomoveisService
+{
+    private readonly IRepositorioGrupoDeAutomovel repositorioGrupo;
+
+    public GrupoDeAutomoveisService(IRepositorioGrupoDeAutomovel repositorioGrupo)
+    {
+        this.repositorioGrupo = this.repositorioGrupo;
+    }
+
+    public Result<GrupoDeAutomoveis> Inserir(GrupoDeAutomoveis grupoDeAutomoveis)
+    {
+        repositorioGrupo.Inserir(grupoDeAutomoveis);
+
+        return Result.Ok(grupoDeAutomoveis);
+    }
+
+    public Result<GrupoDeAutomoveis> Editar(GrupoDeAutomoveis grupoAtualizado)
+    {
+        var grupo = repositorioGrupo.SelecionarPorId(grupoAtualizado.Id);
+
+        if (grupo is null)
+            return Result.Fail("O grupo não foi encontrado");
+
+        grupo.Nome = grupoAtualizado.Nome;
+        
+        repositorioGrupo.Editar(grupo);
+
+        return Result.Ok(grupo);
+    }
+
+    public Result Excluir(int grupoId)
+    {
+        var grupo = repositorioGrupo.SelecionarPorId(grupoId);
+
+        if (grupo is null)
+            return Result.Fail("O grupo não foi Encontrado!");
+        
+        repositorioGrupo.Excluir(grupo);
+
+        return Result.Ok();
+    }
+
+    public Result<GrupoDeAutomoveis> SeleCionarPorId(int grupoId)
+    {
+        var grupo = repositorioGrupo.SelecionarPorId(grupoId);
+
+        if (grupo is null)
+            return Result.Fail("O grupo não foi Encontrado!");
+        
+        return Result.Ok(grupo);
+    }
+
+    public Result<List<GrupoDeAutomoveis>> SelecionarTodos(int usuarioId)
+    {
+        //var grupos = repositorioGrupo
+        //    .Filtrar(f => f.Nome == usuarioId);
+        return Result.Ok();
+    }
+
+}
---
+++ LocadoraDeCarros.Infra.Orm/Migrations/20240821180527_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocadoraDeCarros.Infra.Orm.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBGrupoDeAutomoveis",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBGrupoDeAutomoveis", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBGrupoDeAutomoveis");
+        }
+    }
+}
