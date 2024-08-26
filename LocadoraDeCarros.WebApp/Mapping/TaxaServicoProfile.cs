b61cf14b940dd34f57034b394e2d2af03964260f
MaCout0 <jkmbint@gmail.com>
2024-08-26 13:25:57 -0300

    implementação do mapping

---
+++ LocadoraDeCarros.WebApp/Mapping/TaxaServicoProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using LocadoraDeCarros.Dominio.ModuloTaxaServico;
+using LocadoraDeCarros.WebApp.Models;
+
+namespace LocadoraDeCarros.WebApp.Mapping;
+
+public class TaxaServicoProfile : Profile
+{
+    public TaxaServicoProfile()
+    {
+        CreateMap<InserirTaxaServicoViewModel, TaxaServico>();
+        CreateMap<EditarTaxaServicoViewModel, TaxaServico>();
+        CreateMap<TaxaServico, ListarTaxaServicoViewModel>();
+        CreateMap<TaxaServico, DetalhesTaxaServicoViewModel>();
+        CreateMap<TaxaServico, EditarTaxaServicoViewModel>();
+    }
+}
