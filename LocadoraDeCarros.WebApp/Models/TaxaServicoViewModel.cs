8a5f778e799ce6b39b088fd460e3f6036886038f
MaCout0 <jkmbint@gmail.com>
2024-08-26 13:21:31 -0300

    implementação da model e program

---
+++ LocadoraDeCarros.WebApp/Models/TaxaServicoViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LocadoraDeCarros.WebApp.Models;
+
+public class FormularioTaxaServicoViewModel
+{
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [MinLength(3, ErrorMessage = "O nome deve conter ao menos 3 caracteres")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "A descrição é obrigatória")]
+    public string Descricao { get; set; }
+
+    [Required(ErrorMessage = "O valor é obrigatório")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "O valor deve ser maior que zero")]
+    public decimal Valor { get; set; }
+
+    [Required(ErrorMessage = "O tipo de cobrança é obrigatório")]
+    public int TipoDeCobranca { get; set; }
+
+    public IEnumerable<SelectListItem>? TiposDeCobranca { get; set; }
+}
+
+public class InserirTaxaServicoViewModel : FormularioTaxaServicoViewModel
+{
+}
+
+public class EditarTaxaServicoViewModel : FormularioTaxaServicoViewModel
+{
+    public int Id { get; set; }
+}
+
+public class ListarTaxaServicoViewModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public decimal Valor { get; set; }
+    public string TipoDeCobranca { get; set; }
+}
+
+public class DetalhesTaxaServicoViewModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public decimal Valor { get; set; }
+    public string TipoDeCobranca { get; set; }
+}
--- LocadoraDeCarros.WebApp/Program.cs
+++ LocadoraDeCarros.WebApp/Program.cs
@@ -1,14 +1,17 @@
 using System.Reflection;
 using LocadoraDeCarros.Aplicação.ModuloAutomovel;
 using LocadoraDeCarros.Aplicação.ModuloPlanoCobranca;
+using LocadoraDeCarros.Aplicação.ModuloTaxaServico;
 using LocadoraDeCarros.Aplicação.Servicos;
 using LocadoraDeCarros.Dominio.ModuloGrupoDeAutomovel;
+using LocadoraDeCarros.Dominio.ModuloTaxaServico;
 using LocadoraDeCarros.Dominio.ModuoAutomovel;
 using LocadoraDeCarros.Dominio.PlanoCobranca;
 using LocadoraDeCarros.Infra.Orm.Compartilhado;
 using LocadoraDeCarros.Infra.Orm.ModuloAutomovel;
 using LocadoraDeCarros.Infra.Orm.ModuloGrupoDeAutomovel;
 using LocadoraDeCarros.Infra.Orm.ModuloPlanoCobranca;
+using LocadoraDeCarros.Infra.Orm.ModuloTaxaServico;
 
 namespace LocadoraDeCarros.WebApp;
     
@@ -23,10 +26,12 @@ public class Program
         builder.Services.AddScoped<IRepositorioGrupoDeAutomovel, RepositorioGrupoDeAutomovelEmOrm>();
         builder.Services.AddScoped<IRepositorioAutomovel, RepositorioAutomovelEmOrm>();
         builder.Services.AddScoped<IRepositorioPlanoCobranca, RepositorioPlanoCobrancaEmOrm>();
+        builder.Services.AddScoped<IRepositorioTaxaServico, RepositorioTaxaServicoEmOrm>();
         
         builder.Services.AddScoped<ServicoGrupoDeAutomoveis>();
         builder.Services.AddScoped<ServicoAutomovel>();
         builder.Services.AddScoped<ServicoPlanoCobranca>();
+        builder.Services.AddScoped<ServicoTaxaServico>();
 
         builder.Services.AddAutoMapper(cfg =>
         {
